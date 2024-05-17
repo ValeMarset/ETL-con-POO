53bcf0b14df24cfb62fcb2fcdbd33336815568f7
Valentina Marset <valemarset05@gmail.com>
2024-05-17 17:33:01 -0300

    Limpieza de comentarios

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# Crear un script en python usando __main__ para extraer datos de dos archivos (mibici_12-2014_01-2024.csv,nomenclatura_2024.csv), cada uno con su etl y crear un archivo .csv para cada uno
-# * Un script principal para ejecutar todas las ETLs
-# * Debe ser creado más de un ETL para datasets distintos, (ej. extraer datos de dos apis y crear dos archivos)
-# * Crear una columna con a fecha y hora de extraccion en los dos ETLs.
-# * Intentar reutilizar funciones
-# * Buscar cual la mejor manera de desarollar cada etapa (ETL) las opciones : crear una clase por etapa, o una clase por ETL.
-# * Buenas Praticas de Python (Manifesto y PEP8)
-
-
-# Un script principal para ejecutar los dos ETL.
-# Probá una clase por etapa del ETL.
-# Reutilizar funcciones
-# Usar git o el drive para enviarme la solucción
-
 import argparse
 from class_etl import ETL_Handler
 from class_extraction import Extraction
@@ -56,14 +42,6 @@ def run_etl_nomenclatura():
 
 
 if __name__ == "__main__":
-
-    ## Segunda parte :
-
-    # Implemente argparse y use argumentos para elegir qué ETL queremos ejecutar.
-    # Si ejecuta el script a través de la línea de comando, debe aceptar un argumento con el nombre del ETL.
-    # Mibici o nomenclatura, como desees.
-    # Incluye en la ayuda los nombres de los argumentos y lo que implica cada uno.
-
     parser = argparse.ArgumentParser(description='Script principal para ejecutar diferentes ETLs. Se puede elejir qué ETL ejecutar. Opciones: mibici/nomenclatura')
     parser.add_argument('-etl', type=str, choices=['mibici', 'nomenclatura' ], required=False, help='Indica qué ETL ejecutará')
 
